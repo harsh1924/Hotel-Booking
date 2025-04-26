5edd3848d6dae28082b0eefaefc6ec6785219d8e
harsh1924 <harshrpanwar@gmail.com>
2025-04-26 12:07:37 +0530

    Completed

--- client/src/components/ExclusiveOffers.jsx
+++ client/src/components/ExclusiveOffers.jsx
@@ -4,7 +4,7 @@ import Title from "./Title"
 export const ExclusiveOffers = () => {
     return (
         <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 xl:px-32 pt-20 pb-30">
-            <div className="flex flex-col md:flex-row items-center justify-between">
+            <div className="flex flex-col md:flex-row items-center justify-between w-full">
                 <Title
                     align='left'
                     title='Exclusive Offers'
@@ -20,7 +20,7 @@ export const ExclusiveOffers = () => {
                 </button>
             </div>
 
-            <div className="">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
                 {exclusiveOffers.map(item => (
                     <div
                         className="group relative flex flex-col items-start justify-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center"
@@ -30,7 +30,7 @@ export const ExclusiveOffers = () => {
                         <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full">
                             {item.priceOff}% OFF
                         </p>
-                        <div className="">
+                        <div>
                             <p className="text-2xl font-medium font-playfair">{item.title}</p>
                             <p className="">{item.description}</p>
                             <p className="text-xs text-white/70 mt-3">Expires {item.expiryDate}</p>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Hero } from '../components/Hero'
 import FeaturedDestination from '../components/FeaturedDestination'
 import { ExclusiveOffers } from '../components/ExclusiveOffers'
+import Testimonial from '../components/Testimonial'
+import NewsLetter from '../components/NewsLetter'
 
 export const Home = () => {
   return (
@@ -9,6 +11,8 @@ export const Home = () => {
         <Hero />
         <FeaturedDestination />
         <ExclusiveOffers />
+        <Testimonial />
+        <NewsLetter />
     </>
   )
 }
